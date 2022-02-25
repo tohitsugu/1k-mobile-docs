d12912a1b3a62a0bfcfbb1c0c1cf80858e1427e9
iantfrench <93606113+iantfrench@users.noreply.github.com>
2022-02-25 11:54:14 -0800

    update src

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve("./docs/sidebars.js"),
           // Please change this to your repo.
           // editUrl: "https://github.com/1KBlockID/devx_frontend/tree/develop/", we might want this again someday
         },
